2505c0a220570f53bc96ed2e72bbb93cc77bed3f
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2022-04-13 20:46:17 +0200

    Add modal with in and out animation

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,5 +1,17 @@
 <script>
 	import Board from '$lib/components/game/board/Board.svelte';
+	import Modal from '$lib/components/modal/Modal.svelte';
+
+	let showModal = false;
+	const handleToggleModal = () => {
+		showModal = !showModal;
+	};
 </script>
 
-<Board />
+<button on:click={() => handleToggleModal()}>Open modal</button>
+
+<Modal title="Choose Game Settings" open={showModal} on:close={() => handleToggleModal()}>
+	<svelte:fragment slot="modal-body">
+		<h1>test</h1>
+	</svelte:fragment>
+</Modal>
