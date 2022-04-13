aab4fe7f20919091b36ffd483ac2bb54ebfc7b3a
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2022-04-13 23:51:51 +0200

    added choose game settings using config

--- client/src/lib/config/config.ts
+++ client/src/lib/config/config.ts
@@ -1,1 +1,24 @@
 export const apiBaseUrl = 'http://localhost:4000';
+
+export const gameModes = [
+	{
+		name: 'Bullet',
+		description: 'Each player can have 1 or 2 minutes',
+		times: [1, 2]
+	},
+	{
+		name: 'Blitz',
+		description: 'Each player can have between 3 and 14 minutes',
+		times: [3, 5, 7, 10, 14]
+	},
+	{
+		name: 'Rapid',
+		description: 'Each player can have between 15 and 30 minutes',
+		times: [15, 20, 30]
+	},
+	{
+		name: 'Classical',
+		description: 'Each player can have between 45 and 60 minutes',
+		times: [45, 60]
+	}
+];
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,6 +1,14 @@
-<script>
+<script lang="ts">
 	import Board from '$lib/components/game/board/Board.svelte';
-	import Modal from '$lib/components/modal/Modal.svelte';
+	import Modal from '$lib/layout/modal/Modal.svelte';
+	import { gameModes } from '$lib/config/config';
+
+	let selectedMode: string;
+	let selectedTime: number;
+
+	let times: number[];
+
+	$: times = selectedMode ? gameModes.filter((mode) => mode.name === selectedMode)[0].times : [];
 
 	let showModal = false;
 	const handleToggleModal = () => {
@@ -12,6 +20,20 @@
 
 <Modal title="Choose Game Settings" open={showModal} on:close={() => handleToggleModal()}>
 	<svelte:fragment slot="modal-body">
-		<h1>test</h1>
+		<p>Choose mode</p>
+		<select bind:value={selectedMode}>
+			{#each gameModes as gameMode}
+				<option>{gameMode.name}</option>
+			{/each}
+		</select>
+
+		<p>Select time for each player</p>
+		{#if times}
+			<select bind:value={selectedTime}>
+				{#each times as time}
+					<option>{time}</option>
+				{/each}
+			</select>
+		{/if}
 	</svelte:fragment>
 </Modal>
